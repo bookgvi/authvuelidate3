ad834974971e363663220d175a49cf74eb158e9c
book <bookgvi@gmail.com>
2020-01-05 01:43:05 +0300

    Верстка модального окна бронирований /bookings

--- src/components/Bookings/bookingDialog.vue
+++ src/components/Bookings/bookingDialog.vue
@@ -1,16 +1,45 @@
 <template>
   <b-modal v-model="isShowVM" header-border-variant="white" footer-border-variant="white">
-    <template #modal-header="{ close }" class="pr-0 mr-0">
-      <b-row align-h="between" align-v="center" class="w-100">
-        <b-col cols="11">
-          <span>Номер бронирования: {{ data.id }}</span>
-        </b-col>
-        <b-col cols="1" align="end" class="pr-0">
-          <span class="closeIcon" @click="close()">&times;</span>
-        </b-col>
-      </b-row>
+    <template #modal-header="{ close }">
+      <b-container>
+        <b-row align-v="center">
+          <b-col cols="10">
+            <h4 class="mb-0 font-weight-bold">Бронь {{ data.id }}</h4>
+          </b-col>
+          <b-col cols="2" align="end">
+            <span class="closeIcon" @click="close()">&times;</span>
+          </b-col>
+        </b-row>
+        <b-row class="pb-3">
+          <b-col>
+            <span>Зал</span>
+          </b-col>
+          <b-col align="end">
+            {{ data.room.name }}
+          </b-col>
+        </b-row>
+        <b-row class="pb-3">
+          <b-col>
+            <span>Дата: </span>
+          </b-col>
+          <b-col cols="7" align="end">
+            {{ data.reservedFrom }}
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col>
+            <span>Цель: </span>
+          </b-col>
+          <b-col align="end">
+            {{ data.eventType }}
+          </b-col>
+        </b-row>
+      </b-container>
     </template>
-    Зал: {{ data.room.name }}
+
+    <template v-slot:modal-footer>
+      <span> </span>
+    </template>
   </b-modal>
 </template>
 
